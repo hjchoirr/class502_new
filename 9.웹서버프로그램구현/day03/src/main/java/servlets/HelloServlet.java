424b431cfea0c0830f8e11e7e3fec694c1b13039
hjchoirr <hjchoir@naver.com>
2024-06-11 17:06:08 +0900

    6/11

--- 9.웹서버프로그램구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9.웹서버프로그램구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +10,33 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요");
+        System.out.println("doGet()!!");
+
+        // 모든 servlet이 조회 가능
+        String common1 = req.getServletContext().getInitParameter("common1");
+        String common2 = req.getServletContext().getInitParameter("common2");
+        System.out.printf("common1=%s, common2=%s%n", common1, common2);
+
     }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //HelloServlet 만 조회가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+
+
+    }
+/*
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+ */
 }
---
+++ 9.웹서버프로그램구현/day03/src/main/java/servlets/member/MemberServlet.java
@@ -0,0 +1,22 @@
+package servlets.member;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberServlet-doGet()");
+        System.out.println(req.getRequestURL());
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberServlet-doPost()");
+
+    }
+}
