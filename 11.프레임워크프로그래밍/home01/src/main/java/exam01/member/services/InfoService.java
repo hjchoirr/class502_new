f9bdd43fb99c6bada390ee76a043b2cc8a53adcc
hjchoirr <hjchoir@naver.com>
2024-07-07 22:41:11 +0900

    7/6 home

--- 11.프레임워크프로그래밍/home01/src/main/java/exam01/member/services/InfoService.java
+++ 11.프레임워크프로그래밍/home01/src/main/java/exam01/member/services/InfoService.java
@@ -3,8 +3,10 @@ package exam01.member.services;
 import exam01.member.dao.MemberDao;
 import exam01.member.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -13,10 +15,15 @@ public class InfoService {
     @Autowired
     private MemberDao memberDao;
 
+
     public void printMemberInfo() {
         List<Member> members = memberDao.getList();
-        for(Member member : members) {
-            System.out.println(member);
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy.MM.dd");
+
+        members.forEach(m->{
+            System.out.println(m.getRegDt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            System.out.println(formatter.format(m.getRegDt()));
+            System.out.println(m.getRegDt().format(formatter));
+        });
     }
 }
