a5d5e586510217a8e6d4c1c3736f49ce9b63539a
hjchoirr <hjchoir@naver.com>
2024-09-02 17:59:44 +0900

    9/2 swagger

---
+++ 15.스웨거/project/src/main/java/org/choongang/global/configs/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.choongang.global.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info=@Info(title = "API 명세", description = "설명..", contact = @Contact(name="hjchoi", email="hjchoi@naver.com"), version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi.builder().group("API 명세")
+            .pathsToMatch("/api/v1/**")
+            .build();
+    }
+}
---
+++ 15.스웨거/project/src/main/java/org/choongang/member/entities/Member.java
@@ -0,0 +1,16 @@
+package org.choongang.member.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long seq;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private String userName;
+    private String mobile;
+
+}
